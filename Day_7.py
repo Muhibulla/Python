a86c256c772ff22568e4e15a2a5bdf8065d3e814
Muhibullah <108579671+Muhibulla@users.noreply.github.com>
2023-10-03 21:10:28 +0500

    Add files via upload

---
+++ Day_7.py
@@ -0,0 +1,24 @@
+# Working woth strings
+
+print('This is string with escap\'s charactor')
+
+print('This is string with \n new line charactor.')
+
+print( 'hello ther,\nhow are you?\nI\'m fine thank you.')
+
+# Raw string
+
+print(r'This is a ras\'s string.')
+
+# Indexing and Slicing
+
+text = 'helo world.'
+print(text[5])
+print(text[-3])
+print(text[0:4])
+print(text[:4])
+print(text[4:])
+
+text_h = text[0:4]
+print(text_h)
+
